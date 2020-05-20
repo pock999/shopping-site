dd46dbbae6d2cffd72d35a1da71ff809c3a0a3d4
rick <rickhou44@gmai.com>
2020-05-21 00:59:39 +0800

    add shop page some content

--- src/api/product.js
+++ src/api/product.js
@@ -4,6 +4,9 @@ export default [
     name: '藍色牛仔褲',
     price: 599,
     imageUrl: '@/assets/images/blue_jeans.jpg',
+    detailInfo: {
+      description: '舒服、簡單、寬鬆，簡單搭配就很好看。經典刷白牛仔，適合簡單隨興的你',
+    },
     remainAmount: 5,
   },
   {
@@ -11,6 +14,9 @@ export default [
     name: '深灰直筒牛仔褲',
     price: 699,
     imageUrl: '@/assets/gray_jeans.jpg',
+    detailInfo: {
+      description: '經典直筒牛仔褲，合身又不會不舒服，寬鬆又不會不修身，成熟中帶有一些活力，簡直是今夏潮流聖品',
+    },
     remainAmount: 8,
   },
   {
@@ -18,6 +24,9 @@ export default [
     name: '紅色素T',
     price: 249,
     imageUrl: '@/assets/redT.jpg',
+    detailInfo: {
+      description: '簡單的樣式、大膽的用色，可以低調也可以很高調，非常適合想要簡單又喜氣的你',
+    },
     remainAmount: 12,
   },
   {
@@ -25,6 +34,9 @@ export default [
     name: '深藍大衣(女)',
     price: 1799,
     imageUrl: '@/assets/bbb.jpg',
+    detailInfo: {
+      description: '有著成熟魅力的妳千萬不可以錯過這樣精品，讓您走在時尚的尖端，吸引眾人的目光',
+    },
     remainAmount: 3,
   },
   {
@@ -32,6 +44,9 @@ export default [
     name: '黑色合身T恤',
     price: 299,
     imageUrl: '@/assets/T-shirt-black.jpg',
+    detailInfo: {
+      description: '低調、舒服、輕鬆、簡單，給人一種隨和的感覺，不會過於強眼，同時又帶有一點獨特的魅力',
+    },
     remainAmount: 9,
   },
   {
@@ -39,6 +54,9 @@ export default [
     name: '紅色飛行夾克',
     price: 1200,
     imageUrl: '@/assets/jacket.jpg',
+    detailInfo: {
+      description: '可以防風，也好搭配，簡單的搭配就可以很有魅力，吸引眾人目光，秋冬必備款',
+    },
     remainAmount: 4,
   },
 ];
--- src/views/Shop.vue
+++ src/views/Shop.vue
@@ -1,11 +1,17 @@
 <template>
   <div class="shop">
-    Shoptgreyw
     <div id="product-list">
       <div v-for="item in productList" :key="item.id" class="product-item">
-        {{ item.name }}
-        <div class="product-img-container">
-          <img :src="getImgUrl(item.id)"/>
+        <h2 class="product-name">{{ item.name }}</h2>
+        <div class="product-container">
+          <div class="product-img-container">
+            <img :src="getImgUrl(item.id)"/>
+          </div>
+          <div class="product-info">
+            <div>剩餘數量:{{ item.remainAmount }}</div>
+            <div>NT${{ item.price }}</div>
+            <button class="btn detail-btn">查看詳細資訊</button>
+          </div>
         </div>
       </div>
     </div>
@@ -37,5 +43,35 @@ export default {
     grid-template-columns: 1fr 1fr 1fr;
     grid-row-gap: 20px;
     grid-column-gap: 10px;
+  }
+  .product-container{
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+  }
+  .product-name{
+    font-size: 24px;
+    font-weight: 600;
+  }
+  .product-info{
+    display: flex;
+    flex-direction: column;
+    justify-content:space-around;
+    width: 80%;
+    margin: 0 auto;
+  }
+  .btn{
+    background-color: #268DDD;
+    color: white;
+    border: none;
+    font-size:14px;
+    font-family: '微軟正黑體', Courier, monospace;
+    cursor:pointer;
+  }
+  .btn:hover {
+    background-color: rgb(30, 102, 156);
+  }
+  .detail-btn{
+    padding: 10px 0px;
+    border-radius: 5px;
   }
 </style>
